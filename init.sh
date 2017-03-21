0f9d1c3f40a2294f296d3d8c819947b92dc751a3
khafizovati <khafizova.in@ya.ru>
2017-03-21 14:22:41 +0300

    added files

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -x
+sudo ls -l /home/box/web
+sudo ps -o pid,euser,egroup,comm,args -C nginx
+sudo adduser www-data box
+sudo adduser nginx box
+sudo adduser root box
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/
+sudo rm /etc/nginx/sites-enabled/default
+sudo rm /home/box/web/etc/error.log
+sudo /etc/init.d/nginx restart
+
