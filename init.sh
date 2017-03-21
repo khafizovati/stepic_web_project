e8e00944a2506587a7807b9b1179b38a15dcab43
khafizovati <khafizova.in@ya.ru>
2017-03-21 14:55:28 +0300

    change init.sh&nginx.conf

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ sudo ps -o pid,euser,egroup,comm,args -C nginx
 sudo adduser www-data box
 sudo adduser nginx box
 sudo adduser root box
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo unlink /etc/nginx/sites-enabled/default
 sudo rm /home/box/web/etc/error.log
 sudo /etc/init.d/nginx restart
